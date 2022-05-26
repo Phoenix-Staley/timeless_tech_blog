edf70184c3559b815144764b00661c8d1910a65c
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-26 14:05:13 -0700

    Add seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,35 @@
-const sequelize = require('../config/connection');
-const { User } = require('../models');
+// const connection = require("../config/connection");
+// const { User, Post } = require("../models");
 
-const userData = require('./user_data.json');
+// const userData = require("./user_data.json");
+// const postData = require("./posts.json");
+
+// const seed_database = async () => {
+//     await connection.sync({ force: true });
+
+//     await User.bulkCreate(userData, {
+//         individualHooks: true
+//     });
+
+//     process.exit(0);
+// };
+
+// seed_database();
+
+const sequelize = require("../config/connection");
+const { User, Post } = require("../models");
+
+const userData = require("./user_data.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
