7f726d1bced980dfbf1862e0ac256522499720b8
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-26 22:46:07 -0700

    Add a styled home page

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { Post, User } = require("../models");
 
 router.get("/", async (req, res) => {
-    const postData = await Post.findAll();
+    const postData = await Post.findAll({ include: User});
     const posts = postData.map(post => post.get({ plain: true }));
     res.render("home_dashboard", { posts });
 });
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-// const api_routes = require('./api');
+const api_routes = require('./api');
 const html_routes = require('./html-routes');
 
 router.use('/', html_routes);
-// router.use('/api', api_routes);
+router.use('/api', api_routes);
 
 module.exports = router;
