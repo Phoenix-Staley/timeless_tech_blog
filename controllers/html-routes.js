da6fb9b5e2311f55b2c1e523604cc3e179fbfe5e
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-30 13:55:07 -0700

    Fix single-post rendering

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 const with_auth = require("../utils/auth");
 
-function render_posts(post_data, route, req, res) {
+function render_posts(post_data, req, res) {
     const posts = post_data.map(post => post.get({ plain: true }));
-    res.render(route, { posts: posts, logged_in: req.session.logged_in });
+    res.render("home_dashboard", { posts: posts, logged_in: req.session.logged_in });
 }
 
 router.get("/", async (req, res) => {
     const post_data = await Post.findAll({ include: User});
-    render_posts(post_data, "home_dashboard", req, res);
+    render_posts(post_data, req, res);
 });
 
 router.get("/dashboard", with_auth, async (req, res) => {
@@ -17,7 +17,7 @@ router.get("/dashboard", with_auth, async (req, res) => {
         include: User,
         where: { user_id: req.session.user_id }
     });
-    render_posts(post_data, "home_dashboard", req, res);
+    render_posts(post_data, req, res);
 });
 
 router.get("/user/:id", with_auth, async (req, res) => {
@@ -25,7 +25,7 @@ router.get("/user/:id", with_auth, async (req, res) => {
         include: User,
         where: { user_id: req.params.id }
     });
-    render_posts(post_data, "home_dashboard", req, res);
+    render_posts(post_data, req, res);
 });
 
 router.get("/post/:id", with_auth, async (req, res) => {
@@ -37,7 +37,8 @@ router.get("/post/:id", with_auth, async (req, res) => {
             include: User
         }]
     });
-    render_posts(post_data, "single-post", req, res);
+    const post = post_data.get({ plain: true });
+    res.render("single-post", { post: post, logged_in: req.session.logged_in });
 });
 
 router.get("/login", async (req, res) => {
