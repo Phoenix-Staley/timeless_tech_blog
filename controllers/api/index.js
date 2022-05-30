4ea5d5ab2c793cd7af17800ad4543ee8301659bc
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-30 00:34:25 -0700

    Implement backend handling for new posts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const session_routes = require("./session-routes");
 const comment_routes = require("./comment-routes");
+const new_post_routes = require("./new-post-routes");
 
 router.use("/users", session_routes);
 router.use("/comments", comment_routes);
+router.use("/posts", new_post_routes);
 
 module.exports = router;
---
+++ controllers/api/new-post-routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../../models");
+const with_auth = require("../../utils/auth");
+
+router.post("/new_post", with_auth, async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.post_body) {
+            res.status(400).json({ message: "Posts need a title and a body!" });
+        }
+
+        const new_post = await Post.create({
+            title: req.body.title,
+            body: req.body.post_body,
+            user_id: req.session.user_id
+        });
+
+        res.status(201).json(new_post);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
