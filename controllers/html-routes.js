e4861a038de5095d1f101ff00bfff4ceaacab43c
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-30 13:23:37 -0700

    Put redundant code in a function

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,10 +2,14 @@ const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
 const with_auth = require("../utils/auth");
 
+function render_posts(post_data, route, req, res) {
+    const posts = post_data.map(post => post.get({ plain: true }));
+    res.render(route, { posts: posts, logged_in: req.session.logged_in });
+}
+
 router.get("/", async (req, res) => {
     const post_data = await Post.findAll({ include: User});
-    const posts = post_data.map(post => post.get({ plain: true }));
-    res.render("home_dashboard", { posts: posts, logged_in: req.session.logged_in });
+    render_posts(post_data, "home_dashboard", req, res);
 });
 
 router.get("/dashboard", with_auth, async (req, res) => {
@@ -13,8 +17,15 @@ router.get("/dashboard", with_auth, async (req, res) => {
         include: User,
         where: { user_id: req.session.user_id }
     });
-    const posts = post_data.map(post => post.get({ plain: true }));
-    res.render("home_dashboard", { posts: posts, logged_in: req.session.logged_in })
+    render_posts(post_data, "home_dashboard", req, res);
+});
+
+router.get("/user/:id", with_auth, async (req, res) => {
+    const post_data = await Post.findAll({
+        include: User,
+        where: { user_id: req.params.id }
+    });
+    render_posts(post_data, "home_dashboard", req, res);
 });
 
 router.get("/post/:id", with_auth, async (req, res) => {
@@ -26,8 +37,7 @@ router.get("/post/:id", with_auth, async (req, res) => {
             include: User
         }]
     });
-    const post = post_data.get({ plain: true });
-    res.render("single-post", { post: post, logged_in: req.session.logged_in });
+    render_posts(post_data, "single-post", req, res);
 });
 
 router.get("/login", async (req, res) => {
