a24380767e1096dc79903b4a16c19aa224042ddb
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-30 15:34:25 -0700

    Rename post-routes and add /update/:id path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const session_routes = require("./session-routes");
 const comment_routes = require("./comment-routes");
-const new_post_routes = require("./new-post-routes");
+const new_post_routes = require("./post-routes");
 
 router.use("/users", session_routes);
 router.use("/comments", comment_routes);
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -22,4 +22,23 @@ router.post("/new_post", with_auth, async (req, res) => {
     }
 });
 
+router.post("/update/:id", with_auth, async (req, res) => {
+    try {
+        if (!req.body.title || !req.body.post_body) {
+            res.status(400).json({ message: "Posts need a title and a body!" });
+        }
+
+        const old_post = await Post.findByPk(req.params.id);
+        const updated_post = await old_post.update({
+            title: req.body.title,
+            body: req.body.post_body
+        });
+
+        res.status(201).json(updated_post);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
 module.exports = router;
