a17e1189e1914f83790e2dcae45b6fa008ec2d8a
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-29 23:44:39 -0700

    Add route for user dashboard

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -3,13 +3,22 @@ const { Post, User, Comment } = require("../models");
 const with_auth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-    const postData = await Post.findAll({ include: User});
-    const posts = postData.map(post => post.get({ plain: true }));
+    const post_data = await Post.findAll({ include: User});
+    const posts = post_data.map(post => post.get({ plain: true }));
     res.render("home_dashboard", { posts: posts, logged_in: req.session.logged_in });
 });
 
+router.get("/dashboard", with_auth, async (req, res) => {
+    const post_data = await Post.findAll({
+        include: User,
+        where: { user_id: req.session.user_id }
+    });
+    const posts = post_data.map(post => post.get({ plain: true }));
+    res.render("home_dashboard", { posts: posts, logged_in: req.session.logged_in })
+});
+
 router.get("/post/:id", with_auth, async (req, res) => {
-    const postData = await Post.findByPk(req.params.id, { include: [{
+    const post_data = await Post.findByPk(req.params.id, { include: [{
             model: User
         },
         {
@@ -17,7 +26,7 @@ router.get("/post/:id", with_auth, async (req, res) => {
             include: User
         }]
     });
-    const post = postData.get({ plain: true });
+    const post = post_data.get({ plain: true });
     res.render("single-post", { post: post, logged_in: req.session.logged_in });
 });
 
