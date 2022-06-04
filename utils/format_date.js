fe0593a124536c596faab3701c667c12278b7fa3
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-06-04 13:01:35 -0700

    Fix months appearing incorrectly

--- utils/format_date.js
+++ utils/format_date.js
@@ -1,5 +1,5 @@
 module.exports = function format_date(date) {
-    return `${new Date(date).getMonth()}/${new Date(date).getDate()}/${
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
         new Date(date).getFullYear()
     }`;
 }
